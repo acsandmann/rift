ba95c2d3e9fa5be3e618cf6b744a12c8747cbae8
acsandmann <157552025+acsandmann@users.noreply.github.com>
2025-10-20 23:38:22 -0400

    fix: rely on sls because otherwise drag_swap doesn't work

--- src/bin/rift.rs
+++ src/bin/rift.rs
@@ -177,8 +177,8 @@ Enable it in System Settings > Desktop & Dock (Mission Control) and restart Rift
             CGSEventType::Known(KnownCGSEvent::SpaceWindowDestroyed),
             CGSEventType::Known(KnownCGSEvent::SpaceWindowCreated),
             CGSEventType::Known(KnownCGSEvent::WindowIsChangingScreens),
-            //CGSEventType::Known(KnownCGSEvent::WindowMoved),
-            //CGSEventType::Known(KnownCGSEvent::WindowResized),
+            CGSEventType::Known(KnownCGSEvent::WindowMoved),
+            CGSEventType::Known(KnownCGSEvent::WindowResized),
         ],
         Some(window_tx_store.clone()),
     );
